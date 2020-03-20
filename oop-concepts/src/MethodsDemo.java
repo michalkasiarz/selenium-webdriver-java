da15e1c816bed04a0556377fa7dd80645280f19b
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-20 09:15:59 +0100

    methods demo

---
+++ oop-concepts/src/MethodsDemo.java
@@ -0,0 +1,17 @@
+package src;
+
+public class MethodsDemo {
+
+    static String a = "Hey!";
+
+    public static void main(String[] args) {
+        System.out.println(a);
+
+        method1();
+    }
+
+    public static void method1() {
+        System.out.println(a);
+    }
+
+}
