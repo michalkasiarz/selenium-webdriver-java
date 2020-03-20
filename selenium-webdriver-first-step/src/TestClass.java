217debab5bb13a4e9a4865fad51bb5e70c013b14
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-20 17:46:49 +0100

    Selenium config added

---
+++ selenium-webdriver-first-step/src/TestClass.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestClass {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new FirefoxDriver();
+
+
+    }
+}
