4c46cb0f8c4f34927126f5073225fd27429f1a60
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-20 12:21:13 +0100

    getters and setters first touch

--- getters-and-setters-this-keyword/src/CarWithGettersAndSetters.java
+++ getters-and-setters-this-keyword/src/CarWithGettersAndSetters.java
@@ -1,2 +1,54 @@
 public class CarWithGettersAndSetters {
+
+    // car definition
+    private String color;
+    private String make;
+    private String model;
+    private int year;
+    private int doors;
+
+
+    public void increaseSpeed() {
+        System.out.println("Increasing the speed!");
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
 }
---
+++ getters-and-setters-this-keyword/src/ClassDemo.java
@@ -0,0 +1,18 @@
+public class ClassDemo {
+
+    public static void main(String[] args) {
+
+        CarWithGettersAndSetters bmw = new CarWithGettersAndSetters(); // create
+        // and initialize the object
+        bmw.setColor("Red");
+        bmw.setDoors(4);
+        bmw.setMake("Germany");
+        bmw.setModel("e220");
+        bmw.setYear(1992);
+
+        // method call on bmw object
+        bmw.increaseSpeed();
+
+    }
+
+}
