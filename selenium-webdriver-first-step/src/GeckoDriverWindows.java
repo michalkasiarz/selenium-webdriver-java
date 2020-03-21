019bfb74fd83f17a9397c160828675ef4d5a6a8b
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-21 10:28:41 +0100

    class name refactor

--- selenium-webdriver-first-step/src/GeckoDriverWindows.java
+++ selenium-webdriver-first-step/src/GeckoDriverWindows.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class TestClass {
+public class GeckoDriverWindows {
 
     public static void main(String[] args) {
 
