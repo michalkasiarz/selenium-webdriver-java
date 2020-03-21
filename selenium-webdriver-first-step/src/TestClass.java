d442256cfc38ea041da1721767ea83c6e85444c8
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-21 09:38:20 +0100

    first automated test

--- selenium-webdriver-first-step/src/TestClass.java
+++ selenium-webdriver-first-step/src/TestClass.java
@@ -1,12 +1,26 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestClass {
 
     public static void main(String[] args) {
 
+        System.setProperty("webdriver.gecko.driver", "C:\\libs\\gecko-driver\\geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
-
-
+        String baseURL = "http://redape-recipes.online/";
+        driver.get(baseURL);
+        driver.findElement(By.xpath("//*[@id=\"sidebar-collapse\"]")).click();
+        driver.findElement(By.xpath("//div[@id='mCSB_1_container']/ul//a[@href='/login']")).click();
+        WebElement email = driver.findElement(By.cssSelector("input#email"));
+        WebElement pass = driver.findElement(By.cssSelector("input#password"));
+        WebElement login = driver.findElement(By.xpath("//button[@type='submit']"));
+        email.click();
+        email.sendKeys("test@test.com");
+        pass.click();
+        pass.sendKeys("test@test224");
+        login.click();
+        driver.findElement(By.xpath("//*[@id=\"sidebar-collapse\"]")).click();
     }
 }
