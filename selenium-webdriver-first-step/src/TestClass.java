14553c04c8661ae08cc86f91e328769bd0f2787c
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-21 10:08:18 +0100

    first automated test updated

--- selenium-webdriver-first-step/src/TestClass.java
+++ selenium-webdriver-first-step/src/TestClass.java
@@ -12,15 +12,16 @@ public class TestClass {
         String baseURL = "http://redape-recipes.online/";
         driver.get(baseURL);
         driver.findElement(By.xpath("//*[@id=\"sidebar-collapse\"]")).click();
-        driver.findElement(By.xpath("//div[@id='mCSB_1_container']/ul//a[@href='/login']")).click();
+        WebElement login = driver.findElement(By.xpath("//div[@id='mCSB_1_container']/ul//a[@href='/login']"));
+        login.click();
         WebElement email = driver.findElement(By.cssSelector("input#email"));
         WebElement pass = driver.findElement(By.cssSelector("input#password"));
-        WebElement login = driver.findElement(By.xpath("//button[@type='submit']"));
+        WebElement login_button = driver.findElement(By.xpath("//button[@type='submit']"));
         email.click();
         email.sendKeys("test@test.com");
         pass.click();
         pass.sendKeys("test@test224");
-        login.click();
+        login_button.click();
         driver.findElement(By.xpath("//*[@id=\"sidebar-collapse\"]")).click();
     }
 }
