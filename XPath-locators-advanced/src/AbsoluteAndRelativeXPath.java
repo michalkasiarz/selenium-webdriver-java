1715184e3b47e1002362a7063d7288590d78fce5
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-22 14:16:42 +0100

    working with customized xpath

--- XPath-locators-advanced/src/AbsoluteAndRelativeXPath.java
+++ XPath-locators-advanced/src/AbsoluteAndRelativeXPath.java
@@ -1,5 +1,6 @@
 package src;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,11 +12,12 @@ public class AbsoluteAndRelativeXPath {
     public static void main(String[] args) {
 
         WebDriver driver = new ChromeDriver();
-        String baseURL = "https://learn.letskodeit.com/p/practice";
-
-        driver.manage().window().setSize(new Dimension(600, 400));
+        String baseURL = "https://learn.letskodeit.com/";
+        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get(baseURL);
+
+        driver.findElement(By.xpath("//div[@class='homepage-hero']//a[text()='Enroll now']")).click();
 
 
     }
