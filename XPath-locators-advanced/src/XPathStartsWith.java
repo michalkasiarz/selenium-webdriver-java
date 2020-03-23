283779d2f04c9a9b4e6423a8e3ba1cec30865eb3
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-23 09:06:59 +0100

    customized xpath with starts-with keyword

---
+++ XPath-locators-advanced/src/XPathStartsWith.java
@@ -0,0 +1,24 @@
+package src;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class XPathStartsWith {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        String baseURL = "https://learn.letskodeit.com";
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(baseURL);
+
+        driver.findElement(By.xpath("//div[@id='navbar']//a[starts-with(@class,'navbar-link')]")).click();
+
+
+    }
+
+}
