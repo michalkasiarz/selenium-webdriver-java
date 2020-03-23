dfbce8fac7acce898bc9cb7df23d510431abab5b
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-23 10:47:44 +0100

    introduction to junit

--- junit-intro/test/TestClassTest.java
+++ junit-intro/test/TestClassTest.java
@@ -16,25 +16,23 @@ class TestClassTest {
 
     @Before
     public void setUp() {
-
+        System.out.println("Executed before");
     }
 
     @After
     public void tearDown() {
-
+        System.out.println("Executed after");
     }
 
     @Test
     public void test1() {
-
+        System.out.println("Test1");
     }
 
     @Test
     public void test2() {
-
-
+        System.out.println("Test2");
     }
-
 
 
 }
