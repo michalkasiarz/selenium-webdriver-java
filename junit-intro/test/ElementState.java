0dd06f84928a037fdc868ba2018c797598fca236
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-23 15:30:03 +0100

    element state intro

---
+++ junit-intro/test/ElementState.java
@@ -0,0 +1,36 @@
+package test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ElementState {
+    WebDriver driver;
+    String baseURL;
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        baseURL = "https://www.google.com";
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(baseURL);
+    }
+
+    @Test
+    public void test() {
+        WebElement element = driver.findElement(By.id(""))
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Thread.sleep(3000);
+        driver.quit();
+    }
+}
