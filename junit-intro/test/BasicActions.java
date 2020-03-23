6a6d1fb6d29e57bf4f5a545f8c7af76e284a77e5
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-23 12:25:20 +0100

    first working version of BasicActions junit test

---
+++ junit-intro/test/BasicActions.java
@@ -0,0 +1,32 @@
+package test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class BasicActions {
+
+    WebDriver driver;
+    String baseURL;
+
+    @Before
+    public void setUp() {
+        driver = new FirefoxDriver();
+        baseURL = "http://letskodeit.teachable.com/";
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void test() {
+        driver.get(baseURL);
+    }
+
+    @After
+    public void tearDown() {
+    }
+}
