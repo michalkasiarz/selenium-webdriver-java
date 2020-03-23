7c9e89a195570071ac1b4e4b854369cddc444c05
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-23 08:05:43 +0100

    customized xpath with contains keyword - one more example

---
+++ XPath-locators-advanced/src/XPathContainsKeywordTraining.java
@@ -0,0 +1,23 @@
+package src;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class XPathContainsKeywordTraining {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        String baseURL = "https://learn.letskodeit.com";
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(baseURL);
+
+        driver.findElement(By.xpath("//div[@id='navbar']//a[contains(@class, 'navbar-link') and contains(@href, 'sign_in')]")).click();
+
+    }
+
+}
