b76dff3549174813e98798aaf5c62c4ab3c8cc50
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-26 09:24:45 +0100

    locating radio buttons and clicking on them added

--- junit-intro/test/RadioButtonsAndCheckBoxes.java
+++ junit-intro/test/RadioButtonsAndCheckBoxes.java
@@ -3,7 +3,9 @@ package test;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -24,7 +26,21 @@ public class RadioButtonsAndCheckBoxes {
 
     @Test
     public void test() throws InterruptedException {
+        WebElement bmwRadioBtn = driver.findElement(By.id("bmwradio"));
+        // clicking or BMW radio button
+        bmwRadioBtn.click();
 
+        Thread.sleep(2000);
+
+        WebElement benzRadioBtn = driver.findElement(By.id("benzradio"));
+        // clicking on Benz radio button
+        benzRadioBtn.click();
+
+        Thread.sleep(2000);
+
+        WebElement hondaRadiobtn = driver.findElement(By.id("hondaradio"));
+        // clicking on Honda radio button
+        hondaRadiobtn.click();
     }
 
     @After
