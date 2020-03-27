374d1c52973b2a5e004d415c565508b0cda2355a
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-27 09:51:07 +0100

    multiple select test added

--- junit-intro/test/MultipleSelect.java
+++ junit-intro/test/MultipleSelect.java
@@ -4,9 +4,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.concurrent.TimeUnit;
 
@@ -20,13 +22,36 @@ public class MultipleSelect {
         driver = new FirefoxDriver();
         baseURL = "https://letskodeit.teachable.com/pages/practice";
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
+        driver.manage().window().setSize(new Dimension(1000, 800));
         driver.get(baseURL);
     }
 
     @Test
     public void test() throws InterruptedException {
+        WebElement element = driver.findElement(By.xpath("//select[contains(@name, 'multiple')]"));
+        Select sel = new Select(element);
 
+        Thread.sleep(2000);
+
+        System.out.println("Selecting Orange...");
+        sel.selectByVisibleText("Orange");
+        Thread.sleep(2000);
+        System.out.println("Selecting Peach...");
+        sel.selectByValue("peach");
+        Thread.sleep(2000);
+        System.out.println("Deselecting Orange...");
+        sel.deselectByIndex(1);
+        Thread.sleep(2000);
+        System.out.println("Selecting Apple...");
+        sel.selectByValue("apple");
+        Thread.sleep(2000);
+        System.out.println("Deselecting Peach...");
+        sel.deselectByVisibleText("Peach");
+        Thread.sleep(2000);
+        System.out.println("Selecting both Orange and Peach");
+        sel.selectByValue("orange");
+        sel.selectByValue("peach");
+        Thread.sleep(2000);
     }
 
     @After
