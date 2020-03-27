dedb1a441ecfcaf665503778382bb8d5df117539
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-27 11:09:43 +0100

    clicking on show button and hide button

--- junit-intro/test/ElementDisplayed.java
+++ junit-intro/test/ElementDisplayed.java
@@ -3,7 +3,9 @@ package test;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -19,7 +21,7 @@ public class ElementDisplayed {
         driver = new FirefoxDriver();
         letKodeURL = "http://letskodeit.teachable.com/pages/practice";
         expediaURL = "http://www.expedia.com";
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get(letKodeURL);
     }
@@ -27,6 +29,25 @@ public class ElementDisplayed {
     @Test
     public void testLetsKodeIt() throws InterruptedException {
         driver.get(letKodeURL);
+
+        WebElement textBox = driver.findElement(By.id("displayed-text"));
+        System.out.println("Textbox displayed? " + textBox.isDisplayed());
+
+        Thread.sleep(3000);
+
+        // hide-button
+        WebElement hideButton = driver.findElement(By.id("hide-textbox"));
+        hideButton.click();
+        System.out.println("Clicked on hide button");
+        System.out.println("Textbox displayed? " + textBox.isDisplayed());
+
+        Thread.sleep(3000);
+
+        // show-textbox
+        WebElement showButton = driver.findElement(By.id("show-textbox"));
+        showButton.click();
+        System.out.println("Clicked on show button");
+        System.out.println("Textbox displayed? " + textBox.isDisplayed());
 
     }
 
