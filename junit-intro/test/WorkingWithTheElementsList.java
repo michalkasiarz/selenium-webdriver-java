bf8a681153910ab1a99398f652c444bed92e509b
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-27 07:05:31 +0100

    redundant import deleted

--- junit-intro/test/WorkingWithTheElementsList.java
+++ junit-intro/test/WorkingWithTheElementsList.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
