9006fb1cfc8efad8f25cda74e2e6474b152d354d
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-27 07:27:32 +0100

    template for dropdown elements part added

---
+++ junit-intro/test/DropdownElements.java
@@ -0,0 +1,39 @@
+package test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import java.util.concurrent.TimeUnit;
+
+public class DropdownElements {
+
+    WebDriver driver;
+    String baseURL;
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new ChromeDriver();
+        baseURL = "https://letskodeit.teachable.com/pages/practice";
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(baseURL);
+    }
+
+    @Test
+    public void test() throws InterruptedException {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
+
